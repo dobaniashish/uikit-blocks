161f669689dfa0f6fdda5263e086631a3750a0b7
dobaniashish <dobaniashish@yahoo.com>
2024-07-17 18:09:49 +0530

    refactor: add icon to blocks and make them easily identifiable

--- assets/admin/js/editor.asset.php
+++ assets/admin/js/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'abbf1e66b42dc9ed75ab');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '56a7f621ea072384ccbf');
--- src/js/blocks/button/block.js
+++ src/js/blocks/button/block.js
@@ -1,9 +1,12 @@
 import { registerBlockType } from '@wordpress/blocks';
 
+import { button as icon } from '@wordpress/icons';
+
 import edit from './edit';
 import metadata from './block.json';
 
 registerBlockType( metadata.name, {
 	...metadata,
+	icon,
 	edit,
 } );
