1ca36a73528bc2c86a50cc6a5a33fc95abc1cca0
dobaniashish <dobaniashish@yahoo.com>
2024-07-29 15:16:43 +0530

    refactor: use dropdown instead of popover for button block url input

--- src/js/blocks/button/edit.js
+++ src/js/blocks/button/edit.js
@@ -1,7 +1,5 @@
 import { __ } from '@wordpress/i18n';
 
-import { useEffect, useState } from '@wordpress/element';
-
 import {
 	useBlockProps,
 	RichText,
@@ -14,44 +12,28 @@ import {
 	PanelBody,
 	SelectControl,
 	ToolbarButton,
-	Popover,
 	CheckboxControl,
 	TextControl,
 	Flex,
 	FlexBlock,
+	Dropdown,
 } from '@wordpress/components';
 
-import { link, linkOff } from '@wordpress/icons';
+import { link } from '@wordpress/icons';
 
 import clsx from 'clsx';
 
 import GeneralOptions from '../general-options';
 import generalBlockProps from '../general-block-props';
 
-export default function Edit( { attributes, setAttributes, isSelected } ) {
+export default function Edit( { attributes, setAttributes } ) {
 	const newTabValue = '_blank';
 	const newTabRel = [ 'noreferrer', 'noopener' ];
 	const nofollowRel = 'nofollow';
 
-	const [ popoverAnchor, setPopoverAnchor ] = useState();
-	const [ isEditingURL, setIsEditingURL ] = useState( false );
-	const isURLSet = !! attributes.url;
-
 	const nofollow = attributes.rel
 		? attributes.rel.split( ' ' ).includes( nofollowRel )
 		: false;
-
-	function startEditing( event ) {
-		event.preventDefault();
-		setIsEditingURL( true );
-	}
-
-	function unlink() {
-		setAttributes( {
-			url: undefined,
-		} );
-		setIsEditingURL( false );
-	}
 
 	function updateRel( add = [], remove = [] ) {
 		let relParts = attributes.rel ? attributes.rel.split( ' ' ) : [];
@@ -65,12 +47,6 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 			rel: relParts.join( ' ' ),
 		} );
 	}
-
-	useEffect( () => {
-		if ( ! isSelected ) {
-			setIsEditingURL( false );
-		}
-	}, [ isSelected ] );
 
 	return (
 		<>
@@ -223,161 +199,151 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 			</InspectorControls>
 
 			<BlockControls group="block">
-				{ ! isURLSet && (
-					<ToolbarButton
-						icon={ link }
-						title={ __( 'Link', 'uikit-blocks' ) }
-						onClick={ startEditing }
-					/>
-				) }
-				{ isURLSet && (
-					<ToolbarButton
-						icon={ linkOff }
-						title={ __( 'Unlink', 'uikit-blocks' ) }
-						onClick={ unlink }
-						isActive
-					/>
-				) }
-			</BlockControls>
-
-			{ isSelected && ( isEditingURL || isURLSet ) && (
-				<Popover
-					placement="bottom"
-					anchor={ popoverAnchor }
-					onClose={ () => {
-						setIsEditingURL( false );
-					} }
-					focusOnMount={ isEditingURL ? 'firstElement' : false }
-				>
-					<div style={ { padding: '10px' } }>
-						<Flex direction="column">
-							<FlexBlock>
-								<URLInput
-									label={ __( 'Url', 'uikit-blocks' ) }
-									value={ attributes.url }
-									onChange={ ( value ) => {
-										setAttributes( { url: value } );
-									} }
-									onKeyDown={ () => {
-										setIsEditingURL( true );
-									} }
-									__nextHasNoMarginBottom
-								/>
-							</FlexBlock>
-							<FlexBlock>
-								<SelectControl
-									label={ __( 'Target', 'uikit-blocks' ) }
-									value={ attributes.target }
-									options={ [
-										{
-											label: __(
-												'Same Window',
-												'uikit-blocks'
-											),
-											value: '',
-										},
-										{
-											label: __(
-												'New Window',
-												'uikit-blocks'
-											),
-											value: newTabValue,
-										},
-										{
-											label: __(
-												'Lightbox',
-												'uikit-blocks'
-											),
-											value: 'lightbox',
-										},
-									] }
-									onChange={ ( value ) => {
-										const add =
-											value === newTabValue
-												? newTabRel
-												: [];
-										const remove =
-											value === newTabValue
-												? []
-												: newTabRel;
-										updateRel( add, remove );
-
-										setAttributes( {
-											target: value,
-										} );
-									} }
-								/>
-							</FlexBlock>
-
-							{ attributes.target === 'lightbox' && (
+				<Dropdown
+					renderToggle={ ( { isOpen, onToggle } ) => (
+						<ToolbarButton
+							icon={ link }
+							title={ __( 'Link', 'uikit-blocks' ) }
+							onClick={ onToggle }
+							aria-expanded={ isOpen }
+							isActive={ !! attributes.url }
+						/>
+					) }
+					renderContent={ () => (
+						<div
+							style={ {
+								maxWidth: '100%',
+								minWidth: 'auto',
+								width: '250px',
+							} }
+						>
+							<Flex direction="column">
+								<FlexBlock>
+									<URLInput
+										label={ __( 'Url', 'uikit-blocks' ) }
+										className="ukb-block-editor-url-input"
+										value={ attributes.url }
+										onChange={ ( value ) => {
+											setAttributes( { url: value } );
+										} }
+										__nextHasNoMarginBottom
+									/>
+								</FlexBlock>
 								<FlexBlock>
 									<SelectControl
-										label={ __(
-											'Lightbox Type',
-											'uikit-blocks'
-										) }
-										value={ attributes.lightboxType }
+										label={ __( 'Target', 'uikit-blocks' ) }
+										value={ attributes.target }
 										options={ [
 											{
 												label: __(
-													'Default',
+													'Same Window',
 													'uikit-blocks'
 												),
 												value: '',
 											},
 											{
 												label: __(
-													'Image',
+													'New Window',
 													'uikit-blocks'
 												),
-												value: 'image',
+												value: newTabValue,
 											},
 											{
 												label: __(
-													'Video',
+													'Lightbox',
 													'uikit-blocks'
 												),
-												value: 'video',
-											},
-											{
-												label: __(
-													'Iframe',
-													'uikit-blocks'
-												),
-												value: 'iframe',
+												value: 'lightbox',
 											},
 										] }
 										onChange={ ( value ) => {
+											const add =
+												value === newTabValue
+													? newTabRel
+													: [];
+											const remove =
+												value === newTabValue
+													? []
+													: newTabRel;
+											updateRel( add, remove );
+
 											setAttributes( {
-												lightboxType: value,
+												target: value,
 											} );
 										} }
 									/>
 								</FlexBlock>
-							) }
 
-							<FlexBlock>
-								<CheckboxControl
-									label={ __(
-										'Mark as nofollow',
-										'uikit-blocks'
-									) }
-									checked={ nofollow }
-									onChange={ ( value ) => {
-										const add = value
-											? [ nofollowRel ]
-											: [];
-										const remove = value
-											? []
-											: [ nofollowRel ];
-										updateRel( add, remove );
-									} }
-								/>
-							</FlexBlock>
-						</Flex>
-					</div>
-				</Popover>
-			) }
+								{ attributes.target === 'lightbox' && (
+									<FlexBlock>
+										<SelectControl
+											label={ __(
+												'Lightbox Type',
+												'uikit-blocks'
+											) }
+											value={ attributes.lightboxType }
+											options={ [
+												{
+													label: __(
+														'Default',
+														'uikit-blocks'
+													),
+													value: '',
+												},
+												{
+													label: __(
+														'Image',
+														'uikit-blocks'
+													),
+													value: 'image',
+												},
+												{
+													label: __(
+														'Video',
+														'uikit-blocks'
+													),
+													value: 'video',
+												},
+												{
+													label: __(
+														'Iframe',
+														'uikit-blocks'
+													),
+													value: 'iframe',
+												},
+											] }
+											onChange={ ( value ) => {
+												setAttributes( {
+													lightboxType: value,
+												} );
+											} }
+										/>
+									</FlexBlock>
+								) }
+
+								<FlexBlock>
+									<CheckboxControl
+										label={ __(
+											'Mark as nofollow',
+											'uikit-blocks'
+										) }
+										checked={ nofollow }
+										onChange={ ( value ) => {
+											const add = value
+												? [ nofollowRel ]
+												: [];
+											const remove = value
+												? []
+												: [ nofollowRel ];
+											updateRel( add, remove );
+										} }
+									/>
+								</FlexBlock>
+							</Flex>
+						</div>
+					) }
+				></Dropdown>
+			</BlockControls>
 
 			<div { ...useBlockProps( generalBlockProps( attributes ) ) }>
 				<span
@@ -387,7 +353,6 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 						[ `uk-button-${ attributes.size }` ]: attributes.size,
 						[ `uk-width-${ attributes.width }` ]: attributes.width,
 					} ) }
-					ref={ setPopoverAnchor }
 				>
 					<RichText
 						aria-label={ __( 'Button text', 'uikit-blocks' ) }
